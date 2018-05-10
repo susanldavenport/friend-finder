b5523c6590a0ee9ebd7204be3280d7e9c5b6b382
SusanLDavenport <susanldavenport@gmail.com>
2018-05-09 21:01:39 -0400

    links working + html builds

--- server.js
+++ server.js
@@ -1,1 +1,47 @@
-//Your `server.js` file should require the basic npm packages we've used in class: `express`, `body-parser` and `path`.
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8800;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let friendData = [
+  {
+    name: "susan",
+    urlLink: "https://www.facebook.com/photo.php?fbid=10160419316070224&l=314a5f6199",
+    scores: [1,1,2,5,5,4,3,5,2,4]
+  }
+];
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+
+
+
+
+
+// $.ajax({
+//   url: "/api/matches",
+//   data: JSON.stringify(newFriend),
+//   contentType: "application/json",
+//   method: POST
+// })
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
