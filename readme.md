73a78024a368fc3762cdc9ed7cb03f3e426bbefc
SusanLDavenport <susanldavenport@gmail.com>
2018-05-11 22:36:49 -0400

    js logic, updated data, reworked survey.html

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Friend-Finder
+_Find your next friend! Using Friend-Finder, users are asked to complete a brief 10 question survey about themselves in order to find a new friend with the closest compatability rating._
+
+Each time a user submits their survey responses this app searches through the existing 'Friends List API', calculates the closest match for each response, and provides the user with the name and photo of their new friend. 
+
+![titleImage](/imgs/title.PNG)
+
+## Prerequisites / Dependencies
+To duplicate will need the following things properly installed on your computer.
+* [Git](http://git-scm.com/)
+* [Node.js](http://nodejs.org/) (with NPM)
+
+Node dependencies:
+* express
+* body-parcer
+* mailgun-js
+
+## Installation
+* `git clone <repository-url>` this repository
+* change into the new directory
+* `npm install`
+
+## Running / Development
+* node server.js
+
+## Built With
+- Bootstrap v4.1 - Front-end HTML, CSS, javascript framework for creating responsive web applications
+- JavaScript - Programming language
+- jQuery - Javascript library
+- node.js - Server side programming language
+- express.js - Web application framework used for server side routing
+- body-parser - Middleware parser used for incoming request bodies
+
+## Author
+Susan Davenport
--- server.js
+++ server.js
@@ -2,55 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 8800;
+const app = express();
+const PORT = process.env.PORT || 8800;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-let friendData = [
-  {
-    name: "susan",
-    urlLink: "https://www.facebook.com/photo.php?fbid=10160419316070224&l=314a5f6199",
-    scores: [1,1,2,5,5,4,3,5,2,4]
-  }
-];
+app.use(express.static('imgs'));
 
-const newFriends =[];
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
-
-app.get('api/newFriends', (req, res) => {
-  res.json(friendData); 
-})
-
-app.post('api/newFriends', function(req, res){ 
-  console.log(req.body);
-  const newFriendData = req.body;
-  newFriends.push(newFriendData);
-})
-
-
-
-
-// $.ajax({
-//   url: "/api/matches",
-//   data: JSON.stringify(newFriend),
-//   contentType: "application/json",
-//   method: POST
-// })
-
-
-
-
-
-
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
